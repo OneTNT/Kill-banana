b4f21564e3399b0c8c10d4a4fd2cd51257af1eaf
OneTNT <anonymeuser3@protonmail.com>
2026-04-01 21:23:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const banana = document.getElementById("banana");
 const score_label = document.getElementById("score");
 const aie = new Audio("aie.mp3");
+var play_music = false;
+var multiplicateur = 1;
+const music = new Audio("background_music.mp3");
 
 let score = 0
 
@@ -9,10 +12,19 @@ function mettre_à_jour_score() {
 }
 
 banana.addEventListener('click', function() {
-    score++;
-    mettre_à_jour_score()
+    score += multiplicateur;
+    mettre_à_jour_score();
+    play_background_music();
 });
 
+function play_background_music() {
+    if (play_music == false) {
+        music.loop = true;
+        music.volume = 0.5;
+        music.play();
+        play_music = true;
+    }
+}
 
 setInterval(() => {
   aie.currentTime = 0;
